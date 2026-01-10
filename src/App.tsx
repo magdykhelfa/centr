4ea0fd7c47d7be00e904f965df6775cb2460d2e3
magdykhelfa <magdy.khelfa.eg@gmail.com>
2026-01-10 03:11:05 +0200

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/centr">
         <MainLayout>
           <Routes>
             <Route path="/" element={<Dashboard />} />
